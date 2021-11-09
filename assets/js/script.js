29df44c2e0fe5573a93b93ae71b7cdcc2b8b8b61
Alan Lin <alin6120404@gmail.com>
2021-11-08 17:24:15 -0800

    preventDefault

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+
+var createTaskHandler = function(event) {
+    
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+
+};
 
 //function to call the createTaskHandler
 //listening to the button "click"
-buttonEl.addEventListener("click", createTaskHandler)
+formEl.addEventListener("submit", createTaskHandler);
